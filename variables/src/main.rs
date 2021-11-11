9912dd7f323574fa98f755c957b7c456b7644d64
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-12 00:42:10 +0900

    parseしてみた

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    let x = x * 2;
-
-    println!("The value of x is: {}", x);
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("{}", guess);
 }
