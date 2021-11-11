88fa2ed553dabdf045f8e710fb91e6456610c960
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-11 12:39:27 +0900

    Helloした

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,11 +1,13 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello Fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Hello Frappuccino!!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    // let stdout = stdout();
+    // let message = String::from("Hello Fellow Rustaceans!");
+    // let width = message.chars().count();
+
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(message.as_bytes(), width, &mut writer).unwrap();
 }
