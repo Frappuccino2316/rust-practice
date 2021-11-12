951f6d86b691f7226a4f2b013df638b6f672c38a
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-12 12:48:37 +0900

    関数の戻り値で変数を定義

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    let x = 5;
-    let y = {
-        let x = x-2;
-        x + 1
-    };
+    let x = five();
 
-    another_function(y);
+    println!("x: {}", x);
 }
 
-fn another_function(a: i32) {
-    println!("The a value is: {}", a);
+fn five() -> i32 {
+    5
 }
