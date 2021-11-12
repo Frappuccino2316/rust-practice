54e77306a866c748a31aade5a7d89d63664de60c
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-12 12:38:58 +0900

    引数を複数渡す

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
+fn another_function(x: i32, y: i32) {
     println!("The x value is: {}", x);
+    println!("The y value is: {}", y);
 }
