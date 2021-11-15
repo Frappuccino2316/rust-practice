025f913bed2d0b09aa177fec80787f9583c02335
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-15 23:20:46 +0900

    Stringはムーブされるが、i32はムーブされない

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
+    let s = String::from("Hello");
+    takes_ownership(s);
+
     let x = 5;
-    let y = x;
-    
-    println!("x = {}, y = {}", x, y);
+    makes_copy(x);
+    println!("{}", x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("some_string: {}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("sone_integer: {}", some_integer);
 }
