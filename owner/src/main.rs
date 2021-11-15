a0be69bed559c1182247dd8aa87b0b6b0321e617
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-15 23:44:57 +0900

    参照によって無駄なムーブをなくす

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let s1 = String::from("hellofheafaekrbi");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{}' is {}", s2, len);
+    println!("The length of '{}' is {}", s1, len);
 }
 
-fn calculate_length(s: String) -> (String, usize) {
+fn calculate_length(s: &String) -> usize {
     let length = s.len();
-    (s, length)
+    length
 }
