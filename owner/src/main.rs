70a0342c164a18fa424376397ee57ba83111e5df
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-15 22:57:43 +0900

    無効化された参照は使用できない

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -1,10 +1,5 @@
 fn main() {
-    let mut x = 5;
-    let y = x;
-    println!("{}", x);
-    println!("{}", y);
-
-    let x = 6;
-    println!("{}", x);
-    println!("{}", y);
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}", s2);
 }
