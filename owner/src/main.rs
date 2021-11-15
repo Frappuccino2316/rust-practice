50b5c5c27025019d93ca62c2bd4b0b1d0f1b11b3
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-15 23:09:31 +0900

    整数は値をコピーするのでコピー元の変数は無効化されない

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut s1 = String::from("hello");
-    let s2 = s1.clone();
-    s1 = String::from("world");
-    println!("s1: {}, s2: {}", s1, s2);
+    let x = 5;
+    let y = x;
+    
+    println!("x = {}, y = {}", x, y);
 }
