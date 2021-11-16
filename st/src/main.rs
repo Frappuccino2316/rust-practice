74798fee887d4e2ed6d24fd9453257bb6a227b1a
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-16 22:34:38 +0900

    Userを作る関数を作成

--- st/src/main.rs
+++ st/src/main.rs
@@ -1,19 +1,21 @@
 fn main() {
-    struct User {
-        email: String,
-        username: String,
-        sign_in_count: u64,
-        active: bool,
-    }
-
-    let mut user1 = User {
-        email: String::from("test123@example.com"),
-        username: String::from("test123"),
-        sign_in_count: 1,
-        active: true,
-    };
-
-    user1.username = String::from("test234");
+    let user1 = build_user(String::from("user1@example.com"), String::from("user1"));
 
     println!("{}", user1.username);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        sign_in_count: 1,
+        active: true,
+    }
+}
+
+struct User {
+    email: String,
+    username: String,
+    sign_in_count: u64,
+    active: bool,
 }
