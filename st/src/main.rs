d01cf45acfb619e60c25c4ca9d6f6d09c9893281
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-16 22:04:07 +0900

    インスタンスの内容を変更

--- st/src/main.rs
+++ st/src/main.rs
@@ -6,12 +6,14 @@ fn main() {
         active: bool,
     }
 
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("test123@example.com"),
         username: String::from("test123"),
         sign_in_count: 1,
         active: true,
     };
+
+    user1.username = String::from("test234");
 
     println!("{}", user1.username);
 }
