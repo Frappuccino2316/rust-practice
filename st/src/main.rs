897551329330d8a42267ddf72680c0d0f40b74a5
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-16 13:00:10 +0900

    User構造体を作る

--- st/src/main.rs
+++ st/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    struct User {
+        email: String,
+        username: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let user1 = User {
+        email: String::from("test123@example.com"),
+        username: String::from("test123"),
+        sign_in_count: 1,
+        active: true,
+    };
+
+    println!("{}", user1.username);
 }
