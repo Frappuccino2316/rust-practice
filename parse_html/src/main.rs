dd9f9d00bd133efe14de9622f0bb0119bdfeb8c1
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-25 12:54:13 +0900

    コマンドライン引数を1文字ずつ表示

--- parse_html/src/main.rs
+++ parse_html/src/main.rs
@@ -1,3 +1,14 @@
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Cli = Cli::from_args();
+    let sentence: &String = &args.pattern;
+    for i in sentence.chars() {
+        println!("{}", i);
+    }
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
 }
