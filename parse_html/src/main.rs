2085ebdc8d9841bce897c8fb2fb89b0f3f29364d
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-26 21:01:21 +0900

    parseしたい

--- parse_html/src/main.rs
+++ parse_html/src/main.rs
@@ -3,7 +3,28 @@ use structopt::StructOpt;
 fn main() {
     let args: Cli = Cli::from_args();
     let sentence: &String = &args.pattern;
+    
+    let mut start_tag: String = String::from("");
+    let mut in_tag: bool = false;
+    let mut end_tag: String = String::from("");
+
     for i in sentence.chars() {
+        if i == '<' {
+            start_tag.push(i);
+            in_tag = !in_tag;
+            break
+        }
+        if in_tag {
+            start_tag.push(i);
+            break
+        }
+        if i == '>' {
+            start_tag.push(i);
+            in_tag = !in_tag;
+            break
+        }
+        println!("{}", start_tag);
+        println!("{}", end_tag);
         println!("{}", i);
     }
 }
