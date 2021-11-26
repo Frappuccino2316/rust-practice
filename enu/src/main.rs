bb373076799ea433ad6aa09cc575f93c219dea38
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-27 00:54:40 +0900

    matchでフロー制御する

--- enu/src/main.rs
+++ enu/src/main.rs
@@ -1,5 +1,25 @@
 fn main() {
-    
+    let penny = Coin::Penny;
+
+    let value = value_in_cents(penny);
+    println!("{}", value);
 }
 
-enum 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
