c95b2d014fdd44804305c083bae47d872e6ae197
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-29 20:35:34 +0900

    cargo new

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,32 @@
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     // 絶対パス
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     // 相対パス
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+
+//         // fn seat_at_table() {}
+//     }
+
+//     // mod serving {
+//     //     fn take_order() {}
+
+//     //     fn serve_order() {}
+
+//     //     fn take_payment() {}
+//     // }
+// }
+
+// pub fn eat_at_restaurant() {
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
