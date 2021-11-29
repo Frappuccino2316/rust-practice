553937c9731999120dc6efea857c423716ff0b5e
Frappuccino001 <fumi00hiro@icloud.com>
2021-11-29 20:35:30 +0900

    enumの練習

--- enu/src/main.rs
+++ enu/src/main.rs
@@ -1,25 +1,35 @@
 fn main() {
-    let penny = Coin::Penny;
+    let nickel = Coin::Penny(String::from("Test"));
 
-    let value = value_in_cents(penny);
-    println!("{}", value);
+    // let value = value_in_cents(nickel);
+    // println!("{}", value);
+
+    if let Coin::Nickel(state) = nickel {
+        println!("{}", state);
+    } else {
+        println!("No!!");
+    }
 }
 
 enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
+    Penny(String),
+    Nickel(String),
+    Dime(String),
+    Quarter(String),
 }
 
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
-}
+// fn value_in_cents(coin: Coin) -> u32 {
+    // match coin {
+    //     Coin::Penny => {
+    //         println!("Lucky penny!");
+    //         1
+    //     },
+    //     // Coin::Nickel => 5,
+    //     // Coin::Dime => 10,
+    //     // Coin::Quarter => 25,
+    //     _ => {
+    //         println!("other");
+    //         1000
+    //     },
+    // }
+// }
